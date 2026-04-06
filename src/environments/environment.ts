708aa65b47646de312c6064550e153e3e4c18787
JuanAntonio2025 <jasanchezdec@gmail.com>
2026-04-06 12:22:44 +0200

    Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://tfg-back-main-6zbqvq.laravel.cloud//api/v1',
+  apiBaseUrl: 'https://tfg-back-main-6zbqvq.laravel.cloud/api/v1',
   backendBaseUrl: 'https://tfg-back-main-6zbqvq.laravel.cloud/'
 };
